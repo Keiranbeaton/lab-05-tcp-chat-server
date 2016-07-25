b29415db1681fde6b3abf65b15d345ba8a0e8eec
keiran beaton <keiran beaton>
2016-07-24 18:42:14 -0700

    added gulpfile

---
+++ lab-keiran-beaton/gulpfile.js
@@ -0,0 +1,36 @@
+'use strict';
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+
+var testFiles = ['test/**/*.js'];
+var appFiles = ['lib/**/*.js'];
+gulp.task('lint:app', () => {
+  gulp.src(appFiles)
+    .pipe(eslint({
+      rules: {
+        'indent': [2,2]
+      },
+      envs: [
+        'node',
+        'es6'
+      ]
+    }))
+    .pipe(eslint.format());
+});
+
+gulp.task('lint:test', () => {
+  gulp.src(testFiles)
+    .pipe(eslint({
+      rules: {
+        'indent': [2,2]
+      },
+      envs: [
+        'node',
+        'es6',
+        'mocha'
+      ]
+    }))
+    .pipe(eslint.format());
+});
+
+gulp.task('default', ['lint:app', 'lint:test']);
